9ff4620f4bd1a15306d5b40c5ef12fc016f020b1
Ryoma0305 <ryomaaa0602@gmail.com>
2022-10-26 17:15:59 +0900

    update

--- header.php
+++ header.php
@@ -9,28 +9,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.0/css/all.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<!--lightbox
-<link rel="stylesheet" href="css/lightbox.min.css">
-<script src="js/lightbox.min.js"></script>
--->
-<!-- drawer.css -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.1/css/drawer.min.css">
-<!--/ drawer.css -->
-<!-- jquery & iScroll -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.1.3/iscroll.min.js"></script>
-<!--/ jquery & iScroll -->
-<!-- drawer.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.1/js/drawer.min.js"></script>
-<!--/ drawer.js -->
-<!-- slick -->
-<script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/js/slick.min.js"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/js/slick.css">
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/js/slick-theme.css">
-<!--/ slick -->
-<!-- swiper -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
-<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-<!--/ swiper -->
 <?php
 wp_enqueue_script('jquery');
 wp_head();
@@ -38,38 +16,9 @@ wp_head();
 </head>
 
 <body>
-	<!--
-	<div id="is-loading">
- <div id="loading">
-  <img src="<?php echo get_template_directory_uri() ?>/
-images/Pulse-1s-200px.gif" alt="loading" />
- </div>
-</div>
-	-->
-
 	<section class="mainvisual" <?php body_class(); ?>>
-		 <header>
-			 <div class="header-content">
-				 <h1 class="header-content-logo robot"><a href="<?php echo home_url('/'); ?>">RYOMA ARIMURA</a></h1>
-				 <nav class="header-content-menu robot">
-					 <ul>
-						<li><a href="/#about">ABOUT</a></li>
-						<li><a href="<?php echo home_url('/blog'); ?>">BLOG</a></li>
-						<li><a href="<?php echo home_url('/contact'); ?>">CONTACT</a></li>
-					 </ul>
-				 </nav>
-			</div>
-    <div class="nav-drawer inner">
-      <input id="nav-input" type="checkbox" class="nav-unshown">
-      <label id="nav-open" for="nav-input"><span></span></label>
-      <label class="nav-unshown" id="nav-close" for="nav-input"></label>
-		<h1 class="header-content-logo robot"><a href="<?php echo home_url('/'); ?>">RYOMA ARIMURA</a></h1>
-      <div id="nav-content">
-		  <ul class="robot">
-			  <li><a href="/#about">ABOUT</a></li>
-			  <li><a href="<?php echo home_url('/blog'); ?>">BLOG</a></li>
-			  <li><a href="<?php echo home_url('/contact'); ?>">CONTACT</a></li>
-		  </ul>
+	 <header>
+		<div class="header-content">
+			<h1 class="header-content-logo robot"><a href="<?php echo home_url('/'); ?>">絞り込み検索サンプル</a></h1>
 		</div>
-  </div>
-  </header>
+	</header>
